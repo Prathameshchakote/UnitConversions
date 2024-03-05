30e65d999472a9f07cdfca54e7dfa151f620337b
Prathamesh Chakote <30796045+Prathameshchakote@users.noreply.github.com>
2024-03-05 09:51:52 -0800

    initial changes

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    
+    @State private var input = 0.0
+    @State private var initialUnit = ""
+    @State private var outputUnit = ""
 
-#Preview {
-    ContentView()
-}
+    var units = ["meters", "kilometers", "feet", "yards", "miles"]
+    var output : Double {
+        return input
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+            
+            Form {
+                Section {
+                    TextField("Enter value", value: $input, format: .number)
+                    
+                    Picker("Unit 1", selection: $initialUnit) {
+                        ForEach(units , id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Picker("Unit 2", selection: $outputUnit) {
+                        ForEach(units , id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Text("Change")
+                    
+                    
+                    
+                    }
+                }
+                .navigationTitle("COnvert")
+            }
+            
+        }
+    }
+    
+    #Preview {
+        ContentView()
+    }
