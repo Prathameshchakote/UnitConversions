abce5431e8a86245aa355edd3d45ee04ab16af36
Prathamesh Chakote <30796045+Prathameshchakote@users.noreply.github.com>
2024-03-05 08:53:09 -0800

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Prathamesh on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
